c97f4201c560e066a543774178d24307df069f9e
Vjedi729 <Vjedi729@gmail.com>
2014-01-10 10:06:34 -0600

    Continued work on PickupLifter, added a limit switch.

---
+++ frc2022_2014/src/org/usfirst/frc2022_2014/custom/LimitSwitch.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.usfirst.frc2022_2014.custom;
+/**
+ *
+ * @author Vjedi729
+ */
+public class LimitSwitch {
+    public boolean isPressed(){
+        if(true){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
